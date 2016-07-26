47260e963c833cd6ffc7248cb2a9798c79de1d20
miclp <michaelpeimani@gmail.com>
2016-07-25 17:35:29 -0700

    Add spec.md and application_controller.rb

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,41 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+
+    enable :sessions unless test?
+    set :session_secret, "imnottelling"
+  end
+
+  get '/' do
+    erb :'/index'
+  end
+
+  get '/pry' do
+    binding.pry
+  end
+
+  post '/test' do
+    binding.pry
+    redirect '/'
+  end
+
+  # Helper Methods
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
+    def validate_login
+      redirect "/login" if !logged_in?
+    end
+  end
+
+end
