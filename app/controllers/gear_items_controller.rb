7eeb17cc72e21f9b4ca8c505debcc03d968ac62d
miclp <michaelpeimani@gmail.com>
2016-07-27 12:00:23 -0700

    Add empty framework for controller actions

--- app/controllers/gear_items_controller.rb
+++ app/controllers/gear_items_controller.rb
@@ -1,3 +1,35 @@
 class GearItemsController < ApplicationController
 
+  get '/gear' do
+    validate_login
+    @user = User.find(session[:user_id])
+    erb :'/gear/gear'
+  end
+
+  get '/gear/new' do
+    # create gear item
+    # erb :'/gear/create'
+  end
+
+  post '/gear' do
+    # create gear item
+  end
+
+  patch '/gear' do
+    # update gear item
+  end
+
+  get '/gear/:id' do
+    # erb :'/gear/show_item'
+  end
+
+  get 'gear/:id/edit' do
+    # erb:'/gear/edit'
+  end
+
+  post 'gear/delete' do
+    # 
+  end
+
+
 end
