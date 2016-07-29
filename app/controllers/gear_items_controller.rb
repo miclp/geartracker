5c1c5554210d8a798a70e2daeac50c3ae8af5a3a
miclp <michaelpeimani@gmail.com>
2016-07-29 16:17:31 -0700

    Add create gear item action and view

--- app/controllers/gear_items_controller.rb
+++ app/controllers/gear_items_controller.rb
@@ -8,11 +8,27 @@ class GearItemsController < ApplicationController
 
   get '/gear/new' do
     # create gear item
-    # erb :'/gear/create'
+    erb :'/gear/create'
   end
 
   post '/gear' do
     # create gear item
+    binding.pry
+
+    # create the gear_item and persist to db
+    validate_login
+    @user = User.find(session[:user_id])
+
+    if params[:name] != ""
+      @gear = GearItem.create(name: params[:name], description: params[:description], gear_type: params[:gear_type], value: params[:value], year: params[:year], notes: params[:notes])
+      @gear.user_id = @user.id
+      @gear.save
+    elsif params[:name] == ""
+      redirect "/gear/new"
+    end
+    binding.pry
+    erb :'/gear/gear'
+
   end
 
   patch '/gear' do
@@ -28,7 +44,7 @@ class GearItemsController < ApplicationController
   end
 
   post 'gear/delete' do
-    # 
+    #
   end
 
 
